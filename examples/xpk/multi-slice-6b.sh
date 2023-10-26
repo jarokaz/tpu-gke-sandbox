eca58b53c93d9b3d92e96fd03e8526be9e3f3198
jarokaz <jarokaz@no-reply.com>
2023-10-26 21:34:55 +0000

    wip

---
+++ examples/xpk/multi-slice-6b.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+export LIBTPU_INIT_ARGS="--xla_enable_async_all_gather=true TPU_MEGACORE=MEGACORE_DENSE"
+
+python3 MaxText/train.py MaxText/configs/base.yml run_name=multi-slice-6b-501 dataset_path=gs://jk-gke-aiml-repository/datasets base_output_directory=gs://jk-gke-aiml-repository/runs steps=200 log_period=50 save_period=100 per_device_batch_size=16 dcn_data_parallelism=2 ici_fsdp_parallelism=16 remat_policy=full base_emb_dim=4096 base_num_heads=16 base_mlp_dim=16384 head_dim=256 base_num_decoder_layers=32
+
